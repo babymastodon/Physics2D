bb7f503504b1d0786b3ec237a24fc44bc94be55d
Zack Drach <zdrach@exeter.edu>
2010-05-24 00:44:21 -0400

    random pointer correction, no idea what's going on

--- src/Collision.cpp
+++ src/Collision.cpp
@@ -6,15 +6,15 @@ Collision::Collision(PObject* obj1, PObject* obj2)
 	objects[0] = obj1;
 	objects[1] = obj2;
 	
-	Point* obj1vertices = objects[0]->getVertices();
-	Point* obj2vertices = objects[1]->getVertices();
+	const Point* obj1vertices = objects[0]->getVertices();
+	const Point* obj2vertices = objects[1]->getVertices();
 	
 	float x;
 	float y;
 	
-	for (int i = 0; i < objects[0]->getnumvertices() - 1; i++)
+	for (int i = 0; i < objects[0]->getNumVertices() - 1; i++)
 	{
-		for (int j = 0; j < objects[1]->getnumvertices() - 1; i++)
+		for (int j = 0; j < objects[1]->getNumVertices() - 1; i++)
 		{
 			if ((obj1vertices[i].x < object2vertices[j].x && obj1vertices[i + 1].x > object2vertices[j].x) || (obj1vertices[i].x > object2vertices[j].x && obj1vertices[i + 1].x < object2vertices[j].x))
 			{
@@ -35,7 +35,8 @@ Collision::Collision(PObject* obj1, PObject* obj2)
 		
 	}
 	
-	intersection = new Point(x, y);
+	intersection.x=x;
+	intersection.y=y;
 }
 
 Collision::~Collision(){}
