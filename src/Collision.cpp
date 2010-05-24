c593076807d6f335331cbeda37839efead720700
Nick Lima <nlima@exeter.edu>
2010-05-23 22:17:01 -0400

    started the back end of the collision class

---
+++ src/Collision.cpp
@@ -0,0 +1,24 @@
+#include "Collision.h"
+
+Collision::Collision(PObject* obj1, PObject* obj2)
+{
+	objects = new PObject[2];
+	objects[0] = obj1;
+	objects[1] = obj2;
+	
+	// this needs to calculate the point of intersection between the two objects, this is where the serious collision 
+	// detection can take place 
+}
+
+Collision::~Collision(){}
+
+PObject* Collision::get_objects()
+{
+	return objects;
+}
+
+Point* Collision::get_pointOf()
+{
+	return intersection;
+}
+
---
+++ src/Collision.h
@@ -0,0 +1,20 @@
+#ifndef COLLISION_H
+#define COLLISION_H
+
+#include "Point.h"
+
+class Collision
+{
+public:
+	Collision(PObject* obj1, PObject* obj2);
+	~Collision();
+	
+	Point* get_pointOf();
+	PObject* get_objects();
+	
+private:
+	Point* intersection;
+	PObject* objects[];
+}
+
+#endif
